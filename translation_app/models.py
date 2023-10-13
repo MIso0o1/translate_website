b14973c9ddcdd1e634686e67212af3be15a2d5a4
Michal Somsky <89775059+MIso0o1@users.noreply.github.com>
2023-10-13 16:22:59 +0200

    fix

--- translation_app/models.py
+++ translation_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class TranslatedFile(models.Model):
-    original_file = models.FileField(upload_to='uploads/')
     translated_file = models.FileField(upload_to='translations/')
     target_language = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
