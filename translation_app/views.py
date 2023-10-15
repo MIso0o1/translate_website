bf18af96118ee92782f0cad4fc87626237c9bb73
Michal Somsky <89775059+MIso0o1@users.noreply.github.com>
2023-10-15 12:19:57 +0200

    adding pdf convert

--- translation_app/views.py
+++ translation_app/views.py
@@ -5,9 +5,57 @@ from django.http import HttpResponse
 from .forms import TranslationForm
 from .models import TranslatedFile  # Import your model if not already imported
 from django.shortcuts import render, redirect
+import os
+from django.shortcuts import render
+from pdf2docx import Converter
 
 # Define the API endpoint URL (replace with your actual API URL)
 api_url = "https://translate-documents-79a700ed9476.herokuapp.com/translate"
+
+
+# Function to convert PDF to Word and return the converted file to the user
+import os
+from django.http import HttpResponse
+from django.shortcuts import render
+from pdf2docx import Converter
+
+# Function to convert PDF to Word and return the converted file to the user
+def convert_to_word(request):
+    if request.method == 'POST' and request.FILES['pdf_file']:
+        pdf_file = request.FILES['pdf_file']
+
+        # Save the uploaded PDF file in a designated location
+        pdf_file_path = f'media/{pdf_file.name}'  # Adjust the path as per your project structure
+        with open(pdf_file_path, 'wb+') as destination:
+            for chunk in pdf_file.chunks():
+                destination.write(chunk)
+
+        temp_docx_path = 'media/temp.docx'  # Temporary file path for the converted Word document
+
+        # Convert the PDF file to Word
+        cv = Converter(pdf_file_path)
+        cv.convert(temp_docx_path)
+        cv.close()
+
+        # Read the content of the converted Word document
+        with open(temp_docx_path, 'rb') as file:
+            word_content = file.read()
+
+        # Delete the temporary Word document
+        os.remove(temp_docx_path)
+
+        # Delete the PDF file from the media folder
+        os.remove(pdf_file_path)
+
+        # Set the content type for the response
+        response = HttpResponse(word_content, content_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+
+        # Set the content-disposition header to trigger a download
+        response['Content-Disposition'] = 'attachment; filename="converted_document.docx"'
+        return response
+    else:
+        return HttpResponse("Error: No file uploaded.")
+
 
 def upload_file(request):
     if request.method == 'POST':
