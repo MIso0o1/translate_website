432a51ec303804ed38918859785c6629a988462b
Michal Somsky <89775059+MIso0o1@users.noreply.github.com>
2023-10-22 17:30:58 +0200

    dubug

--- translation_website/settings.py
+++ translation_website/settings.py
@@ -25,7 +25,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = 'django-insecure-9k1x-ncwuuvn#i@3n&hbdf$xzpopxe0r5i0&7q+58pl_=o$hj('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1','www.translatedocuments.net']
 
